ffa6a60bafa72daba8c907192ec176663b672175
0cgr <ozgunluozgurenes@gmail.com>
2024-12-10 10:57:21 +0300

    Add files via upload

---
+++ FoodObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+namespace TutorialVersion
+{
+    public class FoodObject : CellObject
+    {
+        public int AmountGranted = 5;
+        
+        public override void PlayerEntered()
+        {
+            Destroy(gameObject);
+
+            
+            GameManager.Instance.ChangeFood(AmountGranted);
+        }
+    }
+}
